d20d6e299ef9d14ff18945e1c27f3455e3bc4915
Kush21singh <ankush76728@gmail.com>
2023-10-11 08:42:55 +0530

    app1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import Header from "./Header/Header";
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
-    <div>
+    <Fragment>
+      <Cart />
       <Header />
-    </div>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
